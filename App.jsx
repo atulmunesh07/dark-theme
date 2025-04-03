91fb7544bfa7e2621a851a8f7cf5dec960ceadb4
Kumar Atul munesh <131685933+atulmunesh07@users.noreply.github.com>
2025-04-03 16:59:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+const App = () => {
+  return<div className='Container'>
+    
+    <div>
+      <img src="/images/atul.jpg" alt="" className='image-card' />
+    </div>
+    
+
+  </div>
+}
+export default App;
